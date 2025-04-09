b6c6a01ec287cbeee09487a5c3ad129ab3bc645f
jazevedo0406 <joaoazevedo@gmail.com>
2025-04-09 08:46:50 +0100

    Add LiteDB support and enhance server functionality with a user interface
    Updated project files to include LiteDB v5.0.21 for database operations.
    Implemented new features in the Server class for database initialization,
    processing aggregated data, and user interface interactions.
    Introduced SensorData class for data structure and added methods
    for data storage and error handling, improving real-time data
    management from clients.

---
+++ OceanMonitoringSystem/Common/Models/wavyData.cs
@@ -0,0 +1,20 @@
+﻿using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class SensorData
+    {
+        public ObjectId Id { get; set; }
+        public string WavyId { get; set; }
+        public string AggregatorId { get; set; }
+        public string DataType { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string RawValue { get; set; }
+        public DateTime ReceivedAt { get; set; }
+    }
+}
